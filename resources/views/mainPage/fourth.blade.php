3a5d00a8187fff3ac8a2e099b7d0ab735c2a4b86
Unknown <shop4dev@gmail.com>
2017-04-11 20:51:05 +0300

    Finished fourth page layout, fixed the image file

--- resources/views/mainPage/fourth.blade.php
+++ resources/views/mainPage/fourth.blade.php
@@ -1,41 +1,42 @@
 @section('fourth')
 <div class="container" style="position: relative; top: 65px;">
 <div class="row">
-    <div class="col-md-6">
-    <h2>Contact us</h2>
-    <form>
-        
-        <div class="form-group">
-            <div class="col-md-6" style="padding:0">
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Last name">
-            </div>
-            <div class="col-md-6" style="padding:0">
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Last name">
-            </div>
+    <div class="col-md-6" style="text-align:center">
+        <h2 style="font-size: 60px;">Contact us</h2>
+        <form>
             
-        </div>
-        <div class="form-group">
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="E-mail adress">
-        </div>
-        <div class="form-group">
-            <textarea rows="4" cols="10" class="form-control" placeholder="E-mail adress"></textarea>
-        </div>
-        <div class="form-group">
-            <div class="col-md-6" style="padding:0">
-                <button type="submit" class="btn btn-default">Submit</button>
+            <div class="form-group">
+                <div class="col-md-6" style="padding-bottom: inherit; padding: 7px;">
+                    <input type="username" class="form-control" id="exampleInputPassword1" placeholder="First name" style="height: 50px; font-size: 20px;">
+                </div>
+                <div class="col-md-6" style="padding-bottom: inherit; padding: 7px;">
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Last name" style="height: 50px; font-size: 20px;">
+                </div>
             </div>
-            <div class="col-md-6" style="padding:0">
-              <button class="btn btn-success" style="float:right">Reset</button>
-            </div>
+            <div class="form-group">
+                <div class="col-md-12" style="padding-bottom: inherit; padding: 7px;">
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="E-mail adress" style="height: 50px; font-size: 20px;">
+                </div>
             
-        </div>
-      
-    </form>
+            </div>
+            <div class="form-group">
+                <div class="col-md-12" style="padding-bottom: inherit; padding: 7px;">
+                    <textarea rows="4" cols="10" class="form-control" placeholder="Enter your suggestions or questions here"  style="font-size: 20px; height: 333px;"></textarea>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-6" style="padding:7px">
+                    <button class="btn btn-success" style="float:left; width: 213px; height: 45px; font-size: 20px;">Submit</button>
+                </div>
+                <div class="col-md-6" style="padding:7px">
+                <button class="btn btn-success" style="float:right; width: 213px; height: 45px; font-size: 20px;">Reset</button>
+                </div>
+                
+            </div>
+        </form>
     </div>
     <div class="col-md-6">
-        <img src="/main/parallax/images/little_dude.png"/>
-
-        
+        <img src="/main/parallax/images/littleDudeWithQuestionmark.png"/>
     </div>
 </div>
 </div>
